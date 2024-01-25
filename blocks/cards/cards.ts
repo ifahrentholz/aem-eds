f73390c0f2befd137a970db97ca0da9f39c807d7
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-01-25 14:33:47 +0100

    WIP

--- blocks/cards/cards.ts
+++ blocks/cards/cards.ts
@@ -1,6 +1,6 @@
 import { LitElement, css, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import './cards.css';
+import './cards.scss';
 
 /**
  * An example element.
